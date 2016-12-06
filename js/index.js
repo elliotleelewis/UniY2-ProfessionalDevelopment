45336463e4caf3c5088d9aff6bd8040020024015
elliotleelewis <elliot.lewis@hotmail.com>
2016-12-06 02:28:01 +0000

    Added Code Style & Switched to Tabs vs Spaces

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var MainPage = React.createClass({
-    render: function () {
-        return (<h1>Hello World!</h1>);
-    }
+	render: function() {
+		return (<h1>Hello World!</h1>);
+	}
 });
 ReactDOM.render(<MainPage />, $('#reactRoot')[0]);
