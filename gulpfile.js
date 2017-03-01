a0ee32ef51594ddf258c08b08d2e6f64025de953
Elliot Lewis <elliotleelewis@gmail.com>
2017-03-01 19:35:01 +0000

    TravisCI
    Should have seen that one coming…

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,20 @@ const gulp = require('gulp');
 const fs = require('fs');
 const browserify = require('browserify');
 const sass = require('gulp-sass');
+const dir = './build';
+if (!fs.existsSync(dir)){
+	fs.mkdirSync(dir);
+}
 gulp.task('js', function() {
 	return browserify('src/js/index.jsx')
 		.transform("babelify", {presets: ['es2015', 'react']})
 		.bundle()
-		.pipe(fs.createWriteStream('build/js/app.js'));
+		.pipe(fs.createWriteStream(dir + '/js/app.js'));
 });
 gulp.task('sass', function() {
 	return gulp.src('src/css/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('build/css'));
+		.pipe(gulp.dest(dir + '/css'));
 });
 gulp.task('watch', ['default'], function() {
 	gulp.watch(['src/data/*.json', 'src/js/*.jsx'], ['js']);
