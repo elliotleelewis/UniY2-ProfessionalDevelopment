510d7393158a3dda3d61c4df2709f41dc3ee585e
Elliot Lewis <elliotleelewis@gmail.com>
2017-03-11 16:17:11 +0000

    Removed Testing & TravisCI
    Not enough time left in project to do overhauls required for Travis and
    unit testing. Removed from project to save time when committing and
    making pull requests, and to clean up repository.

--- tests/_moduleOptions.test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-global.$ = require('jquery');
-global.jQuery = $;
-global.Tether = require('tether');
-const ModuleOptions = require('../src/js/_moduleOptions');
-test("placeholder", () => {
-	expect(true).toBe(true);
-});
--- tests/_moduleResults.test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-global.$ = require('jquery');
-global.jQuery = $;
-global.Tether = require('tether');
-const ModuleResults = require('../src/js/_moduleResults');
-test("placeholder", () => {
-	expect(true).toBe(true);
-});
